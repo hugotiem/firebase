707a2b08e3eb65be5182aeb6a1374c33c9289d1e
JeanJano <jeansauvage6@gmail.com>
2021-03-07 13:08:28 +0100

    commit

---
+++ lib/Model/Const_soirée.dart
@@ -0,0 +1,65 @@
+const SOIREE_DATA = [
+  {
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },
+  {
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },
+  {
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },
+  {
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },
+  {
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },
+  {
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },{
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },
+
+  {
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },
+  {
+    "nom":"soirée de bertrand",
+    "theme":"classique",
+    "tranche_age":"De 18 à 28",
+    "max":15,
+    "date_heure":"07/04/2021 : 20h30"
+  },
+];
--- lib/View/Pages/Rechercher.dart
+++ lib/View/Pages/Rechercher.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pts/Model/Const_soir%C3%A9e.dart';
 
 class Rechercher extends StatefulWidget {
   @override
@@ -6,10 +7,127 @@ class Rechercher extends StatefulWidget {
 }
 
 class _RechercherState extends State<Rechercher> {
+  ScrollController controller = ScrollController();
+  bool closeTopContainer = false;
+  double topContainer = 0;
+
+  List<Widget> itemsData = [];
+
+  void getPostsData() {
+    List<dynamic> responseList = SOIREE_DATA;
+    List<Widget> listItems = [];
+    responseList.forEach((post) {
+      listItems.add(Container(
+          height: 150,
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20.0)), color: Colors.white, boxShadow: [
+            BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0),
+          ]),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      post["nom"],
+                      style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      post["theme"],
+                      style: const TextStyle(fontSize: 17, color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      post["tranche_age"],
+                      style: const TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      " ${post["max"]}",
+                      style: const TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      post["date_heure"],
+                      style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )));
+    });
+    setState(() {
+      itemsData = listItems;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPostsData();
+    controller.addListener(() {
+      double value = controller.offset/199;
+
+      setState(() {
+        topContainer = value;
+        closeTopContainer = controller.offset > 50;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    final Size size = MediaQuery.of(context).size;
+    final double categoryHeight = size.height*0.30;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          height: size.height,
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                ],
+              ),
+              
+              Expanded(
+                  child: ListView.builder(
+                    controller: controller,
+                      itemCount: itemsData.length,
+                      physics: BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        double scale = 1.0;
+                        if (topContainer > 0.5) {
+                          scale = index + 0.5 - topContainer;
+                          if (scale < 0) {
+                            scale = 0;
+                          } else if (scale > 1) {
+                            scale = 1;
+                          }
+                        }
+                        return Opacity(
+                          opacity: scale,
+                          child: Transform(
+                            transform:  Matrix4.identity()..scale(scale,scale),
+                            alignment: Alignment.bottomCenter,
+                            child: Align(
+                                heightFactor: 1.0,
+                                alignment: Alignment.topCenter,
+                                child: itemsData[index]),
+                          ),
+                        );
+                      })),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
