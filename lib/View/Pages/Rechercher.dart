e96f2c5b6816b5954c784a3d0548cb95315f5825
JeanJano <jeansauvage6@gmail.com>
2021-03-11 22:13:18 +0100

    commit

--- lib/View/Pages/Rechercher.dart
+++ lib/View/Pages/Rechercher.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:pts/Model/Const_soir%C3%A9e.dart';
+
 
 class Rechercher extends StatefulWidget {
   @override
@@ -7,6 +10,203 @@ class Rechercher extends StatefulWidget {
 }
 
 class _RechercherState extends State<Rechercher> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(  
+        slivers: [
+          SliverPersistentHeader(
+            pinned: true,
+            floating: false,
+            delegate: SearchHeader(
+              icon: Icons.party_mode,
+              title: 'Rechercher',
+              search: _Search(),
+            ),
+          ),
+        SliverFillRemaining(
+          hasScrollBody: true,
+          child: Scroll(),
+        )
+        ],
+      ),
+    );
+  }
+}
+
+class SearchHeader extends SliverPersistentHeaderDelegate{
+  final double minTopBarHeight = 100;
+  final double maxTopBarHeight = 200;
+  final String title;
+  final IconData icon;
+  final Widget search;
+
+  SearchHeader({
+    @required this.title,
+    this.icon,
+    this.search,
+  });
+
+  @override 
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    var shrinkFactor = min(1, shrinkOffset / (maxExtent - minExtent));
+
+    var topBar = Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        alignment: Alignment.center,
+        height:
+            max(maxTopBarHeight * (1 - shrinkFactor * 1.45), minTopBarHeight),
+        width: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(title,
+                style: Theme.of(context).textTheme.headline4.copyWith(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            SizedBox(
+              width: 20,
+            ),
+            Icon(
+              icon,
+              size: 40,
+              color: Colors.white,
+            )
+          ],
+        ),
+        decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(36),
+              bottomRight: Radius.circular(36),
+            )),
+      ),
+    );
+    return Container(
+      height: max(maxExtent - shrinkOffset, minExtent),
+      child: Stack(
+        fit: StackFit.loose,
+        children: [
+          if (shrinkFactor <= 0.5) topBar,
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: 10,
+              ),
+              child: Container(
+                alignment: Alignment.center,
+                child: search,
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 10,
+                        color: Colors.green.withOpacity(0.23),
+                      )
+                    ]),
+              ),
+            ),
+          ),
+          if (shrinkFactor > 0.5) topBar,
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 230;
+
+  @override
+  double get minExtent => 100;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
+}
+
+
+class _Search extends StatefulWidget {
+  _Search({Key key}): super(key: key);
+
+  @override
+  __SearchState createState() => __SearchState();
+}
+
+class __SearchState extends State<_Search> {
+  TextEditingController _editingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _editingController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _editingController,
+              onChanged: (_) => setState(() {}),
+              decoration: InputDecoration(  
+                hintText: "Rechercher",
+                hintStyle: TextStyle(  
+                  color: Colors.black,
+                ),
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              ),
+            ),
+          ),
+          _editingController.text.trim().isEmpty
+            ? IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.black.withOpacity(0.5),
+              ),
+               onPressed: null)
+            : IconButton( 
+                highlightColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                icon: Icon(
+                  Icons.clear,
+                  color: Colors.black.withOpacity(0.5)
+                ),
+                onPressed: () => setState(  
+                  () {
+                    _editingController.clear();
+                  }
+                ),
+            )
+      ],
+      ),
+    );
+  }
+}
+
+
+class Scroll extends StatefulWidget {
+  @override
+  _ScrollState createState() => _ScrollState();
+}
+
+class _ScrollState extends State<Scroll> {
   ScrollController controller = ScrollController();
   bool closeTopContainer = false;
   double topContainer = 0;
@@ -82,7 +282,6 @@ class _RechercherState extends State<Rechercher> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    final double categoryHeight = size.height*0.30;
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
