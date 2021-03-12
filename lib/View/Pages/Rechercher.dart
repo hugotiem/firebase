272d74685f569de0020de0c510aa9f1227ec79e2
JeanJano <jeansauvage6@gmail.com>
2021-03-12 11:53:48 +0100

    commit

--- lib/View/Pages/Rechercher.dart
+++ lib/View/Pages/Rechercher.dart
@@ -81,7 +81,7 @@ class SearchHeader extends SliverPersistentHeaderDelegate{
           ],
         ),
         decoration: BoxDecoration(
-            color: Colors.lightBlue,
+            color: Colors.lightGreen,
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(36),
               bottomRight: Radius.circular(36),
