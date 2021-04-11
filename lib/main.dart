fcd7563fd9c60334bb3dc0ff3a4e1355b67e8498
yoggy <hugotiem@gmail.com>
2021-04-11 14:21:29 +0200

    adding firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 import 'View/Home.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   static final String title = "Upload Flutter To GitHub";
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "PTS",
       //theme: ,
-      home: Home(),
+      home: FutureBuilder(
+        future: _fbApp,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("ERROR");
+            return Text("ERROR");
+          } else if (snapshot.hasData) {
+            return Home();
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
