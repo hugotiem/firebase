96addf7f3571b5a6cb210fddc05cc2de4259331c
yoggy <hugotiem@gmail.com>
2021-04-11 17:37:34 +0200

    working on login page

--- lib/View/Pages/login/login.dart
+++ lib/View/Pages/login/login.dart
@@ -11,6 +11,8 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -27,7 +29,37 @@ class _LoginState extends State<Login> {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: Container(),
+      body: Container(
+        height: size.height - 40,
+        width: size.width,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(right: 20, left: 20),
+                width: size.width - 40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 10,
+                      color: Colors.blue.withOpacity(0.23),
+                    )
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(border: InputBorder.none),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
