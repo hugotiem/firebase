d43ab3f6753d77b26231f1ffa31050a7b91b96c2
yoggy <hugotiem@gmail.com>
2021-04-19 01:04:42 +0200

    create static class Soiree

--- lib/Model/soiree.dart
+++ lib/Model/soiree.dart
@@ -1,16 +1,21 @@
 class Soiree {
-  String nom;
-  String theme;
-  double nombre;
-  double age;
-  DateTime date;
+  static String nom;
+  static String theme;
+  static double nombre;
+  static double age;
+  static DateTime date;
 
+  static void setDataFistPage(String _nom, String _theme, double _nombre) {
+    nom = _nom;
+    theme = _theme;
+    nombre = _nombre;
+  }
 
-  Soiree( 
-    this.nom,
-    this.theme,
-    this.nombre,
-    this.age,
-    this.date,
-  );
+  // attribut heure à rajouter
+  static void setDataSecondPage(DateTime _date) {
+    date = _date;
+  }
+
+  // attributs pour l'adress de la soirée à rajouter
+  static void setDataThirdPage() {}
 }
