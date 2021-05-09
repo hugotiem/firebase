bf72f10d83f9ef56eb43f2373e86c14bd032ca25
yoggy <hugotiem@gmail.com>
2021-05-09 18:56:50 +0200

    try colors

--- lib/Constant.dart
+++ lib/Constant.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 const Color FOCUS_COLOR = Color(0xFFE7E7E7);
 
 // PRINCIPAL COLORS
-const Color ICONCOLOR = Color(0xffffcc5c);
+const Color ICONCOLOR = Color(0xffFFC300);
 const Color PRIMARY_COLOR = Color(0xFFF5F5F5);
-const Color SECONDARY_COLOR = Colors.black;
-
+const Color SECONDARY_COLOR = Color(0xff232f3e);
