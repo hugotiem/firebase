2aea646e4b2b37bf7f9023eeec3f879fcb9bc3ec
yoggy <hugotiem@gmail.com>
2021-05-10 13:56:15 +0200

    test scroll effect on profil page for ios devices

--- lib/View/Pages/profil/Profil.dart
+++ lib/View/Pages/profil/Profil.dart
@@ -25,161 +25,165 @@ class _ProfilState extends State<Profil> {
         backgroundColor: PRIMARY_COLOR,
         brightness: Brightness.light,
       ),
-      body: Container(
-        color: PRIMARY_COLOR,
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              ClickableContainer(
-                to: ProfilDetails(),
-                containerShadow: true,
-                child: Row(
-                  children: <Widget>[
-                    ProfilPhoto(),
-                    Container(
-                      margin: EdgeInsets.only(left: 20),
-                      child: new Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          new Text(
-                            "Name",
-                            style: new TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          Opacity(
-                            opacity: 0.7,
-                            child: new Text("Afficher le profil"),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              PTSBox(
-                padding: EdgeInsets.only(top: 20, bottom: 20),
-                // creer une list avec tous les elements
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(left: 20, bottom: 10),
-                      child: new Text("Paramètres du compte"),
-                    ),
-                    ClickableContainer(
-                      to: Login(),
-                      margin: EdgeInsets.only(top: 10),
-                      padding: EdgeInsets.only(
-                        top: 0,
-                        bottom: 0,
-                        left: 20,
-                        right: 20,
-                      ),
-                      child: new Container(
-                        padding: EdgeInsets.only(
-                          top: 15,
-                          bottom: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: Colors.grey.withOpacity(0.23),
-                            ),
+      body: ListView(
+        children: [
+          Container(
+            color: PRIMARY_COLOR,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  ClickableContainer(
+                    to: ProfilDetails(),
+                    containerShadow: true,
+                    child: Row(
+                      children: <Widget>[
+                        ProfilPhoto(),
+                        Container(
+                          margin: EdgeInsets.only(left: 20),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Text(
+                                "Name",
+                                style: new TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Opacity(
+                                opacity: 0.7,
+                                child: new Text("Afficher le profil"),
+                              ),
+                            ],
                           ),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            new Text(
-                              "Informations personnelles",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            new Icon(Icons.perm_identity)
-                          ],
-                        ),
-                      ),
+                      ],
                     ),
-                    ClickableContainer(
-                      to: API(),
-                      padding: EdgeInsets.only(
-                        top: 0,
-                        bottom: 0,
-                        left: 20,
-                        right: 20,
-                      ),
-                      child: new Container(
-                        padding: EdgeInsets.only(
-                          top: 15,
-                          bottom: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: Colors.grey.withOpacity(0.23),
-                            ),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            new Text(
-                              "Paiements",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            new Icon(Icons.payment)
-                          ],
-                        ),
-                      ),
-                    ),
-                    ClickableContainer(
-                      padding: EdgeInsets.only(
-                        top: 0,
-                        bottom: 0,
-                        left: 20,
-                        right: 20,
-                      ),
-                      child: new Container(
-                        padding: EdgeInsets.only(
-                          top: 20,
-                          bottom: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(
-                              color: Colors.grey.withOpacity(0.23),
-                            ),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            new Text(
-                              "Notifications",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            new Icon(Icons.notifications_outlined)
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              ClickableContainer(
-                containerShadow: true,
-                child: Text(
-                  "Se deconnecter",
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 20,
                   ),
-                ),
+                  PTSBox(
+                    padding: EdgeInsets.only(top: 20, bottom: 20),
+                    // creer une list avec tous les elements
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(left: 20, bottom: 10),
+                          child: new Text("Paramètres du compte"),
+                        ),
+                        ClickableContainer(
+                          to: Login(),
+                          margin: EdgeInsets.only(top: 10),
+                          padding: EdgeInsets.only(
+                            top: 0,
+                            bottom: 0,
+                            left: 20,
+                            right: 20,
+                          ),
+                          child: new Container(
+                            padding: EdgeInsets.only(
+                              top: 15,
+                              bottom: 15,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.grey.withOpacity(0.23),
+                                ),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                new Text(
+                                  "Informations personnelles",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                new Icon(Icons.perm_identity)
+                              ],
+                            ),
+                          ),
+                        ),
+                        ClickableContainer(
+                          to: API(),
+                          padding: EdgeInsets.only(
+                            top: 0,
+                            bottom: 0,
+                            left: 20,
+                            right: 20,
+                          ),
+                          child: new Container(
+                            padding: EdgeInsets.only(
+                              top: 15,
+                              bottom: 15,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.grey.withOpacity(0.23),
+                                ),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                new Text(
+                                  "Paiements",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                new Icon(Icons.payment)
+                              ],
+                            ),
+                          ),
+                        ),
+                        ClickableContainer(
+                          padding: EdgeInsets.only(
+                            top: 0,
+                            bottom: 0,
+                            left: 20,
+                            right: 20,
+                          ),
+                          child: new Container(
+                            padding: EdgeInsets.only(
+                              top: 20,
+                              bottom: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: Colors.grey.withOpacity(0.23),
+                                ),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                new Text(
+                                  "Notifications",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                new Icon(Icons.notifications_outlined)
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ClickableContainer(
+                    containerShadow: true,
+                    child: Text(
+                      "Se deconnecter",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
