54067a4f395bb8554632e66ab6373b5898a709d0
JeanJano <jeansauvage6@gmail.com>
2021-07-13 01:08:34 +0200

    custom agenda

--- lib/View/Pages/Planning/components/CalendarWidget.dart
+++ lib/View/Pages/Planning/components/CalendarWidget.dart
@@ -56,6 +56,7 @@ class _CalendarWidgetState extends State<CalendarWidget> {
       child: Column(
         children: [
           Expanded(
+            flex: 4,
             child: SfCalendar(
               onTap: calendarTapped,
               monthViewSettings: MonthViewSettings(
@@ -75,34 +76,72 @@ class _CalendarWidgetState extends State<CalendarWidget> {
             ),
           ),
           Expanded(  
-            child: Container(  
+            child: Container(
+              padding: EdgeInsets.only(top: 8), 
               color: Colors.white,
               child: ListView.separated(
                 itemCount: _appointmentDetails.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    height: 50,
-                    color: Colors.white,
-                    child: ListTile(  
-                      leading: Column(
-                        children: [
-                          Text(
-                            '${DateFormat('hh:mm a').format(_appointmentDetails[index].from)}'
-                          )
-                        ]
+                  return Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Column(
+                            children: [
+                              Text(
+                                '${DateFormat.E('fr_FR').format(_appointmentDetails[index].from)}',
+                              ),
+                              Text(
+                                '${DateFormat.d('fr_FR').format(_appointmentDetails[index].from)}'
+                              ),
+                            ]
+                          ),
+                        )
                       ),
-                    ),
+                      Expanded(
+                        flex: 5,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 4, left: 4),
+                          child: Container(
+                            height: 50,
+                            decoration: BoxDecoration(  
+                              color: SECONDARY_COLOR,
+                              borderRadius: BorderRadius.circular(5)
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  _appointmentDetails[index].eventName,
+                                  style: TextStyle(  
+                                    color: ICONCOLOR
+                                  ),
+                                ),
+                                Text(
+                                  '${DateFormat.Hm('fr_FR').format(_appointmentDetails[index].from)}',
+                                  style: TextStyle(  
+                                    color: ICONCOLOR
+                                  ),
+                                )
+                              ]
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]
                   );
                 }, 
                 separatorBuilder: (BuildContext context, int index) =>
-                const Divider(
-                  height: 5,
-                ) 
+                  const Divider(
+                    height: 5,
+                  )
               ),
             ),
-          )
-        ],
-      ),
+          ),
+        ]
+      )
     );
   }
 
