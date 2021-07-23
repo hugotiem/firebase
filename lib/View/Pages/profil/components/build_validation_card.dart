6e44f28313bac61f69392e987d25e22d699d7557
JeanJano <jeansauvage6@gmail.com>
2021-07-23 18:12:40 +0200

    working on wait list

--- lib/View/Pages/profil/components/build_validation_card.dart
+++ lib/View/Pages/profil/components/build_validation_card.dart
@@ -1,17 +1,74 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:pts/Model/components/pts_box.dart';
+import 'package:pts/Model/services/auth_service.dart';
+import 'package:pts/View/Pages/profil/components/title_text_profil.dart';
 
-import 'card_validation_detail.dart';
 
 Widget buildValidationCard(BuildContext context, DocumentSnapshot party) {  
-  int itemCount = party['wait list'].length;
+  String name = party['Name'];
+
+  Stream<QuerySnapshot> getNameList(BuildContext context) async* {
+  yield* FirebaseFirestore.instance
+      .collection('party')
+      .where('UID', isEqualTo: AuthService.currentUser.uid)
+      .where(party['wait list'][0]['party name'], isEqualTo: party['Name'])
+      .snapshots();
+  }
+
   return Stack(
     children: [
-      CardValidation(
-        title: party['Name'],
-        name: party['wait list'].toString(),
-        itemCount: itemCount,
-      ),
+      Center(
+        child: PTSBox(
+          padding: EdgeInsets.only(top: 20, bottom: 20),
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TitleTextProfil(
+                text: name
+              ),
+              TextButton(
+                onPressed: () {
+                  print(party['wait list'][0]['party name']);
+                }, 
+                child: Text(
+                  'test'
+                )
+              ),
+              StreamBuilder(
+                stream: getNameList(context),
+                builder: (context, snapshot) {
+                  if(!snapshot.hasData) return Center(child: const CircularProgressIndicator());
+                  return ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (BuildContext context, int index) =>
+                      buildNameList(context, snapshot.data.docs[index])
+                  );
+                }
+              )
+            ],
+          ),
+        ),
+      )
     ]
   );
 }
+
+
+
+Widget buildNameList(BuildContext context, DocumentSnapshot party) {
+  return Row(  
+      children: [
+        Container(
+          height: 50,
+          child: Text(
+            party['wait list'].toString(),
+          ),
+        )
+      ],
+    );
+}
+
