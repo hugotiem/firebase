9fc77bb9cba2d745e6c8085f69111f30953329a0
Hugo Tiem <hugotiem@gmail.com>
2021-08-21 13:50:32 +0200

    LoginCubit completed

--- lib/Model/services/auth_service.dart
+++ lib/Model/services/auth_service.dart
@@ -18,42 +18,42 @@ class AuthService extends ChangeNotifier {
     isLogged = val;
   }
 
-  Future<Map<String, User>> register(String _email, String _password) async {
+  Future<User> register(String _email, String _password) async {
     try {
       UserCredential userCredential =
           await _auth.createUserWithEmailAndPassword(
         email: _email,
         password: _password,
       );
-      return {"success": userCredential.user};
+      return userCredential.user;
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        return {'The password provided is too weak.': null};
-      } else if (e.code == 'email-already-in-use') {
-        return {'The account already exists for that email.': null};
-      } else if (e.code == 'invalid-email') {
-        return {'Adresse email invalide.': null};
-      }
-      return {e.code: null};
+      // if (e.code == 'weak-password') {
+      //   return 'The password provided is too weak.';
+      // } else if (e.code == 'email-already-in-use') {
+      //   return 'The account already exists for that email.';
+      // } else if (e.code == 'invalid-email') {
+      //   return 'Adresse email invalide.';
+      // }
+      throw e;
     }
   }
 
-  Future<Map<String, User>> signIn(String _email, String _password) async {
+  Future<User> signIn(String _email, String _password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: _email,
         password: _password,
       );
-      return {"success": userCredential.user};
+      return userCredential.user;
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        return {'No user found for that email.': null};
-      } else if (e.code == 'wrong-password') {
-        return {'Wrong password provided for that user.': null};
-      } else if (e.code == 'invalid-email') {
-        return {'Adresse email invalide.': null};
-      }
-      return {e.code: null};
+      // if (e.code == 'user-not-found') {
+      //   return 'No user found for that email.';
+      // } else if (e.code == 'wrong-password') {
+      //   return 'Wrong password provided for that user.';
+      // } else if (e.code == 'invalid-email') {
+      //   return 'Adresse email invalide.';
+      // }
+      throw e;
     }
   }
 
---
+++ lib/blocs/login/login_state.dart
@@ -0,0 +1,29 @@
+part of 'login_cubit.dart';
+
+enum LoginStatus { initial, logging, signedUp, logged }
+
+class LoginState extends AppBaseState<LoginStatus> {
+  const LoginState(LoginStatus status,
+      {bool requestInProgress = false,
+      String requestFailureCode,
+      String requestFailureMessage})
+      : super(
+            status: status,
+            requestInProgress: requestInProgress,
+            requestFailureCode: requestFailureCode,
+            requestFailureMessage: requestFailureMessage);
+
+  const LoginState.initial() : this(LoginStatus.initial);
+  const LoginState.signedUp() : this(LoginStatus.signedUp);
+  const LoginState.logged() : this(LoginStatus.logged);
+
+  @override
+  AppBaseState<LoginStatus> copyWith(
+          {bool requestInProgress = false,
+          String requestFailureCode,
+          String requestFailureMessage}) =>
+      LoginState(status,
+          requestInProgress: requestInProgress,
+          requestFailureCode: requestFailureCode,
+          requestFailureMessage: requestFailureMessage);
+}
