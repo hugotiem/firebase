f23feb331b17b3a0d40d1a1f0182a0818709ed1b
JeanJano <jeansauvage6@gmail.com>
2021-12-14 11:56:25 +0100

    working on ui profil page

--- lib/View/Pages/profil/subpage/user_page.dart
+++ lib/View/Pages/profil/subpage/user_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:pts/Constant.dart';
 import 'package:pts/components/back_appbar.dart';
-import 'package:pts/components/pts_box.dart';
+import 'package:pts/Constant.dart';
 
 class ProfilDetails extends StatefulWidget {
   ProfilDetails({Key? key}) : super(key: key);
@@ -50,117 +50,95 @@ class _ProfilDetailsState extends State<ProfilDetails> {
           //brightness: Brightness.dark,
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              PTSBox(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  "AuthService.auth.currentUser.displayName",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 22,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                ),
-                                Opacity(
-                                  opacity: 0.5,
-                                  child: Text(
-                                    "A rejoint en ",//${_month[_creationDate.month - 1]} ${_creationDate.year}",
-                                    style: TextStyle(fontSize: 18),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Container(
-                            height: 80,
-                            width: 80,
-                            clipBehavior: Clip.antiAlias,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.blue.withOpacity(0.5),
-                            ),
-                            child: GestureDetector(
-                              onTap: () {},
-                              child: Image.network(
-                                'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png',
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 30),
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            child: Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.star_rate_rounded,
-                                  color: Colors.orange,
-                                ),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Opacity(
-                                  opacity: 0.7,
-                                  child: Text(
-                                    "3 avis",
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            child: Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.check_rounded,
-                                  color: Colors.green,
-                                ),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Opacity(
-                                  opacity: 0.7,
-                                  child: Text(
-                                    "Identité verifiée",
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeadProfil(
+              fullName: 'Jean Sauvage',
+              age: '21',
+              photo: "assets/roundBlankProfilPicture.png",
+              identiteVerif: 'Identité vérifiée',
+              avis: '0',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HeadProfil extends StatelessWidget {
+  final String? fullName;
+  final String? age;
+  final String? photo;
+  final String? identiteVerif;
+  final String? avis;
+
+  const HeadProfil({this.fullName, this.age, this.photo, this.avis, this.identiteVerif, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: CircleAvatar(
+              radius: 60,
+              backgroundImage: AssetImage(photo!),
+            ),
+          ),
+        ),
+        Text(
+          fullName!,
+          style: TextStyle(
+              fontWeight: FontWeight.w500,
+              color: SECONDARY_COLOR,
+              fontSize: 22),
+        ),
+        Opacity(
+          opacity: 0.65,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 12),
+            child: Text(
+              '$age ans',
+              style: TextStyle(color: SECONDARY_COLOR, fontSize: 16),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 40, bottom: 15),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 22),
+                child: Icon(Icons.star, color: ICONCOLOR),
               ),
-              PTSBox(),
+              Text('$avis avis',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: SECONDARY_COLOR,
+                    fontWeight: FontWeight.w100,
+                  ))
             ],
           ),
         ),
-      ),
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 22),
+              child: Icon(Icons.verified_user_sharp, color: ICONCOLOR),
+            ),
+            Text(identiteVerif!,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: SECONDARY_COLOR,
+                  fontWeight: FontWeight.w100,
+                ))
+          ],
+        ),
+      ],
     );
   }
 }
