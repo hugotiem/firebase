7f6c121166303a1846d8654c7056e04ea345be24
hugocaptain <hugo@captain-team.net>
2022-06-05 16:58:52 +0200

    adding title to appbar

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -4,36 +4,35 @@ import '../components/party_card/party_export.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final void Function()? onPressed;
-  const CustomAppBar({Key? key, this.onPressed}) : super(key: key);
+  final String? title;
+  const CustomAppBar({Key? key, this.onPressed, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leadingWidth: 70,
-        leading: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: IconButton(
-            icon: Image.asset("assets/back-btn.png"),
-            onPressed: onPressed
-              
+      backgroundColor: Colors.transparent,
+      title: title != null ? Text(title!) : null,
+      elevation: 0,
+      leadingWidth: 70,
+      leading: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: IconButton(
+            icon: Image.asset("assets/back-btn.png"), onPressed: onPressed),
+      ),
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              SECONDARY_COLOR,
+              ICONCOLOR,
+            ],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: const [0.0, 1.0],
           ),
         ),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                SECONDARY_COLOR,
-                ICONCOLOR,
-              ],
-              begin: const FractionalOffset(0.0, 0.0),
-              end: const FractionalOffset(1.0, 0.0),
-              stops: const [0.0, 1.0],
-            ),
-          ),
-        ),
-      );
+      ),
+    );
   }
 
   @override
