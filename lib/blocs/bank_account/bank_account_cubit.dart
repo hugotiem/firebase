617c076a6d99bec49bddb560db5234f339d776cc
JeanJano <jeansauvage6@gmail.com>
2022-06-07 20:12:41 +0200

    update pakage

--- lib/blocs/bank_account/bank_account_cubit.dart
+++ lib/blocs/bank_account/bank_account_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:pts/blocs/base/app_base_cubit.dart';
 import 'package:pts/blocs/base/app_base_state.dart';
 import 'package:pts/models/payments/bank_account.dart';
-import 'package:pts/services/firestore_service.dart';
 import 'package:pts/services/payment_service.dart';
 
 part 'bank_account_state.dart';
@@ -10,7 +9,6 @@ class BankAccountCubit extends AppBaseCubit<BankAccountState> {
   BankAccountCubit() : super(BankAccountState.initial());
 
   final PaymentService _paymentService = PaymentService();
-  final FireStoreServices _firestore = FireStoreServices("user");
 
   Future<void> loadData(String? userId) async {
     if (userId == null) return emit(BankAccountState.failed());
